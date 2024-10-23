f8b477e17c597937a2f02fd8946b2002d5762638
21prnv <pranavpatil6251@gmail.com>
2024-10-23 10:18:16 +0530

    added signin controller

--- backend/controllers/signin.controller.js
+++ backend/controllers/signin.controller.js
@@ -1,16 +1,41 @@
-const signin = (req,res)=>{
-    const { email, password } = req.body
-    try{
-        res.status(201).json({
-            success: true,
-            message: 'You are signed in'
-        })
-    }catch(err){
-        res.status(500).json({
-            success: false,
-            message: `${err}, Server error`
-        })
-    }
-}
+const UserModel = require("../models/users.model");
+const bcrypt = require("bcrypt");
 
-module.exports = signin
+const signin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = UserModel.findOne({ email: email });
+
+    if (user) {
+      const isPassWordMatched = await bcrypt.compare(password, user.password);
+      if (isPassWordMatched) {
+        res.status(201).json({
+          success: true,
+          message: "Password Matched!",
+        });
+      } else {
+        res.status(500).json({
+          success: true,
+          message: "Password Not Matched!",
+        });
+      }
+
+      res.status(201).json({
+        success: true,
+        message: "You are signed in",
+      });
+    } else {
+      res.status(500).json({
+        success: false,
+        message: `User Not Exist Server error`,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: `${err}, Server error`,
+    });
+  }
+};
+
+module.exports = signin;
