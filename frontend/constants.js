c9e852c16e711b059075a1f4e0db0963b37a9110
nissha29 <nishakashyap2907@gmail.com>
2024-12-18 20:44:48 +0530

    change to PROD link

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,4 +1,4 @@
-// const URL = 'https://checkpoint-fjy6.onrender.com'
-const URL = 'http://localhost:8000'
+const URL = 'https://checkpoint-fjy6.onrender.com'
+// const URL = 'http://localhost:8000'
 export default URL
 
